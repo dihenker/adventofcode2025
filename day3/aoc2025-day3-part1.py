d93a49ecd8a5cdebca4e3f336684af016ffe85bb
Om Dihenker <om.dihenker@gmail.com>
2025-12-03 13:24:27 -0500

    day 3 part 1 simplified to single loop

--- day3/aoc2025-day3-part1.py
+++ day3/aoc2025-day3-part1.py
@@ -13,6 +13,7 @@ def partOne(file):
             for i, v in enumerate(bank):
                 if i == len(bank) - 1: # cant have last digit be a left digit
                     break
+                
                 if int(v) > leftDigit:
                     leftDigit = int(v)
                     rightDigit = int(bank[i+1])
@@ -24,4 +25,27 @@ def partOne(file):
     
     return sum(maxes)
 
+def partOneSimplified(file):
+    maxes = []
+
+    with open(file) as f:
+        for bank in f:
+            bank = bank.strip()
+            leftDigit = 0
+            rightDigit = 0
+
+            for i, v in enumerate(bank):
+                if int(v) > leftDigit and i != len(bank) - 1: # left digit needs to change, right auto changes
+                    leftDigit = int(v)
+                    rightDigit = int(bank[i+1])
+                elif int(v) > rightDigit: # left digit no change, check right digit for bigger
+                    rightDigit = int(v)
+                    
+            
+            
+            maxes.append(leftDigit*10 + rightDigit)
+    
+    return sum(maxes)
+
 print(partOne("input.txt"))
+print(partOneSimplified("input.txt"))
