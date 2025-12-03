696cc78096a5d28fd9d2a69ce3fe5ac71898ee3f
Om Dihenker <om.dihenker@gmail.com>
2025-12-03 13:04:59 -0500

    day 3 part 2 passed

--- day3/aoc2025-day3-part2.py
+++ day3/aoc2025-day3-part2.py
@@ -1,5 +1,9 @@
 # adventofcode 2025 day 3 part 2
 
+# look for max digit between (prev_selected_digit_index + 1) (starts at 0, we add 1 for start of next digit index) 
+# and (length - remaining_number_of_digits_we_are_looking_for)
+# ensures we have remaining digits and we preserver order
+
 def partTwo(file):
     sum = 0
 
@@ -12,22 +16,21 @@ def partTwo(file):
             prevMaxDigitIndex = 0
             for d in range(12):
                 d = 11 - d # reverse from 11 to 0
-                maxDigit = 0
+                maxDigit = 0 
 
-                print(prevMaxDigitIndex, bankLen-d-1)
-                # currentBank = bank[prevMaxDigitIndex: bankLen-d] # using this breaks the original index
+                # from prev selected digit index to just before remaining number of digits
+                # e.g. third digit, need at least 9 digits left over to use
                 for i in range(prevMaxDigitIndex, bankLen-d):
-                    print(i)
                     batteryInt = int(bank[i])
-
+                    # taking max from available set (i.e. prevMaxDigitIndex to bankLen-d)
                     if batteryInt > maxDigit:
                         maxDigit = batteryInt
-                        prevMaxDigitIndex = i+1 # start of next iter
+                        prevMaxDigitIndex = i + 1 # plus 1 for start of sublist for next digit search
                 
-                currentSum = currentSum*10 + maxDigit
+                currentSum = currentSum * 10 + maxDigit
             sum += currentSum
     
     return sum
 
 
-print(partTwo("test.txt"))
+print(partTwo("input.txt"))
