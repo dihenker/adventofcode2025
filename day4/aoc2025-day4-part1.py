ea8210065d0401c342917d4914ceb9a3738181bc
Om Dihenker <om.dihenker@gmail.com>
2025-12-04 13:15:22 -0500

    day 4 part 1 passed

---
+++ day4/aoc2025-day4-part1.py
@@ -0,0 +1,53 @@
+# adventofcode 2025 day 4 part 1
+
+def partOne(file):
+    # directions to search, i.e. adjacent squares
+    directions = (
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1)
+        )
+
+    # create grid
+    grid = []
+    with open(file) as f:
+        for line in f:
+            line = line.strip() # strip the newline
+            row = []
+            for i in line:
+                row.append(i)
+            grid.append(row)
+    
+    numRows = len(grid)
+    numCols = len(grid[0])
+
+    accessible = 0
+    for m, row in enumerate(grid):
+        for n, square in enumerate(row):
+            if square == ".": # skip none roll square
+                continue
+
+            numAdjacent = 0
+            for i, j in directions:
+                if 0 <= n+i and n+i < numCols \
+                    and 0 <= m+j and m+j < numRows:
+                    if grid[m+j][n+i] == "@":
+                        numAdjacent += 1
+
+                    if numAdjacent >= 4:
+                        break
+            
+            if numAdjacent < 4:
+                accessible += 1
+    
+    return accessible
+
+
+    
+
+print(partOne("input.txt"))
